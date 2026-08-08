788a2cc343324d10f7cc1f1b0a2e2a3c19714c2d
Ozod <ozodhamzayev10@gmail.com>
2026-08-08 13:28:17 +0500

    string

--- string/main.go
+++ string/main.go
@@ -1,7 +1,8 @@
 package main
 
-
-import ("fmt")
+import (
+	"fmt"
+)
 
 
 func main() {
@@ -9,9 +10,41 @@ func main() {
 	word := []string{"H","a","n","n","a","h"}
 	result := ReverseString(word)
 	fmt.Println(result)
+
+	str := "((()))"
+	resultValid := Is_valid(str)
+	fmt.Println(resultValid)
+
+
+
+}
+
+//! 3
+
+
+//! 4
+func Is_valid(s string) bool {
+	count1:= 0
+	count2 := 0
+	leng := len(s)-1
+	if s[leng] == 40 {
+		return false
+	}
+	for i := 0; i < len(s); i++ {
+		if s[i] == 40 {
+			count1++
+		} else if s[i] == 41 {
+			count2++
+		}
+	}
+	if count1 == count2 {
+		return true
+	}
+	return false
 }
 
 
+//! 5
 func  ReverseString(s []string) []string {
 	result := []string{}
 	for i := len(s)-1; i >= 0; i-- {
