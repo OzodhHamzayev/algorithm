2c36e8d95690fa84dc91638c3f0b1c891d0576ce
Ozod <ozodhamzayev10@gmail.com>
2026-08-13 13:55:44 +0500

    15

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //! 1
-// TODO: 2 ta loop ishlatganimning sababi — value[0] va value[10]  dagi sonlarning yig‘indisi target ga teng bo‘lishi mumkin. 
+// TODO: 2 ta loop ishlatganimning sababi — value[0] va value[10]  dagi sonlarning yig‘indisi target ga teng bo‘lishi mumkin.
 // TODO Ular ketma-ket kelmasligi mumkin. Agar kerakli sonlar ketma-ket  kelganida, bitta loopdan foydalanardim.
 func TwoNumberSum(array []int, target int) []int {
 	lens := len(array)-1
@@ -117,29 +119,29 @@ func SortedSquares(nums []int) []int {
 //! 7
 
 
-func LeftRightDifference(nums []int) []int {
-	result := []int{}
-	for i := 0; i < len(nums); i++ {
-		left := 0
-		right := 0
+// func LeftRightDifference(nums []int) []int {
+// 	result := []int{}
+// 	for i := 0; i < len(nums); i++ {
+// 		left := 0
+// 		right := 0
 
-		for k := 0; k < len(nums); k++ {
-			if k < i {
-				left += nums[k]
-			} else if k > i {
-				right += nums[k]
-			}
-		}
-		if left > right {
-			result = append(result, -1)
-		} else if left < right {
-			result = append(result, 1)
-		} else {
-			result = append(result, 0)
-		}
-	}
-	return result
-}
+// 		for k := 0; k < len(nums); k++ {
+// 			if k < i {
+// 				left += nums[k]
+// 			} else if k > i {
+// 				right += nums[k]
+// 			}
+// 		}
+// 		if left > right {
+// 			result = append(result, -1)
+// 		} else if left < right {
+// 			result = append(result, 1)
+// 		} else {
+// 			result = append(result, 0)
+// 		}
+// 	}
+// 	return result
+// }
 
 //! 8
 
@@ -198,27 +200,276 @@ func  BestSeat(seats []int) int {
 	return -1
 }
 
-//! 10
+//! 10 ->
+
+
+func removeDuplicates(nums []int) int {
+
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[k] == nums[i] {
+			continue
+		} else {
+			nums[k+1], nums[i] = nums[i], nums[k+1]
+			k = k+1
+		}
+	}
+	return k+1
+}
+
+
+
+
+//! 11
+
+func MoveZeroes2(nums []int) []int {
+	zero := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[zero] = nums[zero], nums[i]
+			zero++
+		}
+	}
+	return nums
+}
+
+//! 12
+
+func MoveElementToEnd2(array []int, toMove int) []int {
+	target := 0
+	for i := 0; i < len(array); i++ {
+		if array[i] != toMove {
+			array[i], array[target] = array[target], array[i]
+			target++
+		}
+	}
+	return array
+}
+
+//! 12-2
+
+func MoveElementToEnd3(array []int, toMove int) []int {
+	left, mid, right := 0,0, len(array)-1
+	for mid <= right {
+		if array[mid] == toMove {
+			mid++
+		} else {
+			array[mid], array[left] = array[left], array[mid]
+			left++
+			mid++
+		}
+	}
+	return array
+}
+
+
+//! 13 
+
+func MaxArea(nums []int) int {
+	left, right := 0, len(nums)-1
+	waterResult := 0
+	for left <= right { 
+		water := 0
+		if nums[left] >= nums[right] {
+			water = nums[right] * (right-left)
+		} else {
+			water = nums[left] * (right-left)
+		}
+		if waterResult < water {
+		waterResult = water
+		}
+		if nums[left] > nums[right] {
+			right--
+		} else if nums[left] < nums[right] {
+			left++
+		} else {
+			left++
+			right--
+		}
+	}
+	return waterResult
+}
+
+//! 17
+
+
+
+func sortingElement(nums []int) []int {
+	 low, high := 0, len(nums)-1
+	 mid := (high+low)/2
+	 for low <= high {
+		if nums[mid] < nums[low] {
+			nums[mid], nums[low] = nums[low], nums[mid]
+			mid--
+			fmt.Println(nums)
+
+		} else if nums[mid] > nums[high] {
+			nums[mid], nums[high] = nums[high], nums[mid]
+			mid++
+		} else {
+			break
+		}
+	 }
+	 return nums
+}
+
+
+//! 14 
+
+  func FirstDuplicateValue(array []int) int {
+	m := make(map[int]bool)
+	for i := 0; i < len(array); i++ {
+
+		if m[array[i]] == true {
+			return array[i]
+		}
+		m[array[i]] = true
+		
+	}
+    return -1
+  }
+
+//! 15  -> o(n kvadrat)
+
+func ZeroSumSubarray(nums []int) bool {
+	for i := 0; i < len(nums); i++ {
+	sum := 0
+		for k := i; k < len(nums); k++ {
+			sum += nums[k]
+			if sum == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+
+//! 16 -> o(n kvadrat)
+
+
+func  FirstNonRepeatingCharacter(str string) int {
+	m := make(map[byte]bool)
+	for i := 0; i < len(str); i++ {
+
+		for k := i+1; k < len(str); k++ {
+			if str[i] == str[k] {
+				m[str[i]] = true
+			} 
+		}
+		if m[str[i]] == false  {
+			return i
+		}
+	}
+
+	return -1
+}
+//! 17 -> o(n)
+func  FirstNonRepeatingCharacter2(str string) int {
+    m := make(map[byte]int)
+
+    for i := 0; i < len(str); i++ {
+        m[str[i]]++
+    }
+
+		for i := 0; i < len(str); i++ {
+			if m[str[i]] == 1 {
+				return i
+			}
+	}
+	return -1
+}
+
+//! 18 
+
+func LargestRange(array []int) []int {
+	min := array[0]
+	max := 0
+	result := make([]int, 2)
+	for i := 0; i < len(array); i++ {
+		for k := 0; k < len(array); k++ {
+			if min >= array[k] && i == 0 {
+				min = array[k]
+				result[0] = min
+				max = min
+				max++
+				} else if max == array[k] {
+				max, result[1] = array[k], array[k]
+				max++
+			}
+		}
+	}
+	return result
+}
 
 
 
 func main() {
 
-	seats := []int{1,1,1}
-	resultSeats := BestSeat(seats)
-	fmt.Println(resultSeats)
+
+	nums := []int{4, 2, 1, 3, 6}
+	result := LargestRange(nums)
+	fmt.Println(result)
+
+
+
+
+	// word := "a,b,c,a,b"
+	// result := FirstNonRepeatingCharacter2(word)
+	// fmt.Println(result)
+
+
+
+	// word := "aaaaaaaaaaaaaaaaaaaabbbbbbbbbbcccccccccccdddddddddddeeeeeeeeffghgh"
+	// result := FirstNonRepeatingCharacter(word)
+	// fmt.Println(result)
+
+
+	// nums := []int{-5, -5, 2, 3, -2}
+	// resultNums := ZeroSumSubarray(nums)
+	// fmt.Println(resultNums)
+
+
+	// nums := []int{2, 1, 5, 2, 3, 3, 4}
+	// resultNums := FirstDuplicateValue(nums)
+	// fmt.Println(resultNums)
+
+
+
+	// nums := []int{2, 1, 5, 2, 3, 3, 4}
+	// resultNums := sortingElement(nums)
+	// fmt.Println(resultNums)
+
+	// water := []int{1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1}
+	// waterResult := MaxArea(water)
+	// fmt.Println(waterResult)
+
+
+
+	// array := []int{1, 2, 4, 5, 6}
+	// targetElement := 3
+	// resulstElement := MoveElementToEnd3(array, targetElement)
+	// fmt.Println(resulstElement)
+
+
+	// moveZero := []int{1,0,0,3,12}
+	// resultZero := MoveZeroes2(moveZero)
+	// fmt.Println(resultZero)
+
+	// duplicates := []int{0,0,0,0}
+	// resultDuplicates := removeDuplicates(duplicates)
+	// fmt.Print(resultDuplicates,"\n")
+
+
+	// seats := []int{1,1,1}
+	// resultSeats := BestSeat(seats)
+	// fmt.Println(resultSeats)
 
 
 
 	// moton := []int{1, 2, 3, 3, 2, 1}
 	// resultMoton := IsMonotonic(moton)
 	// fmt.Println(resultMoton)
-
-
-	// numDifference := []int{1,2,3,4}
-	// resultDifference := LeftRightDifference(numDifference)
-	// fmt.Println(resultDifference)
-
 
 
 	// numUnsort := []int{-4,-23,0,3,10}
--- string/main.go
+++ string/main.go
@@ -4,25 +4,7 @@ import (
 	"fmt"
 )
 
-
-func main() {
-	 
-	word := []string{"H","a","n","n","a","h"}
-	result := ReverseString(word)
-	fmt.Println(result)
-
-	str := "((()))"
-	resultValid := Is_valid(str)
-	fmt.Println(resultValid)
-
-
-
-}
-
-//! 3
-
-
-//! 4
+//! 1
 func Is_valid(s string) bool {
 	count1:= 0
 	count2 := 0
@@ -44,11 +26,57 @@ func Is_valid(s string) bool {
 }
 
 
-//! 5
+//! 2
 func  ReverseString(s []string) []string {
 	result := []string{}
 	for i := len(s)-1; i >= 0; i-- {
 		result = append(result, s[i])
 	}
 	return result
+}
+
+//! 3
+
+func  IsPalindrome(str string) bool {
+	found := false
+	left, right := 0, len(str)-1
+
+
+	for left <= right {
+		if str[left] == str[right] {
+			left++
+			right--
+			found = true
+		} else { 
+			found = false
+			return found
+		}
+	}
+	return found
+}
+//! 4 
+
+
+
+func main() {
+	 
+
+
+
+	// palindrome := "a"
+	// resultPalindrome := IsPalindrome(palindrome)
+	// fmt.Println(resultPalindrome)
+
+	// word := []string{"H","a","n","n","a","h"}
+	// result := ReverseString(word)
+	// fmt.Println(result)
+
+
+
+	// str := "((()))"
+	// resultValid := Is_valid(str)
+	// fmt.Println(resultValid)
+
+
+
 }
